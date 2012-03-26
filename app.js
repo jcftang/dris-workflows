15a495da6e5ec72a3d887686430576031fd4a025
matthias van wambeke <matthias.van.wambeke@howest.be>
2012-03-26 11:38:24 +0100

    Made a 404 page and did the proper routing in app.js

--- app.js
+++ app.js
@@ -18,17 +18,26 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser({keepExtensions: true}));
   app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
   app.use(log4js.connectLogger(logger, { level: log4js.levels.INFO }));
 });
 
-app.configure('development', function(){
+/*app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler());
+});*/
+
+app.error(function(err, req, res, next){
+	console.log(err);
+    if (err instanceof NotFound) {
+        res.render('404',{id:"404",title:"404 - Not found"});
+    } else {
+        next(err);
+    }
 });
 
 
@@ -48,12 +57,25 @@ app.post('/create/item',routes.createItem);
 app.post('/create/serie',routes.createSerie)
 app.get('/list/images/:id',routes.getItemImages);
 app.get('/remove',routes.remove);
-app.get('/remove/item/:id',routes.removeItem)
+app.get('/remove/item/:id',routes.removeItem);
+
 // Redirects
 app.get('/', function(req, res){
 	res.redirect('/home');
 });
 
+function NotFound(msg){
+  this.name = 'NotFound';
+  Error.call(this, msg);
+  Error.captureStackTrace(this, arguments.callee);
+}
+
+NotFound.prototype.__proto__ = Error.prototype;
+
+app.get('/*', function(req, res){
+	console.log(req.url)
+  throw new NotFound;
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
