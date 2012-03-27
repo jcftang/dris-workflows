71a29db0e63ba55743df797e1cca78d339efc012
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-03-27 09:39:29 +0100

    fix up temp test

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ app.get('/*', function(req, res){
 	throw new NotFound;
 });
 
+module.exports = app;
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
