3f78f4d57ba49fa1ce41f23c40ab3c3e4f38366b
matthias van wambeke <matthias.van.wambeke@howest.be>
2012-04-02 13:00:41 +0100

    reformatting suggestions

--- app.js
+++ app.js
@@ -52,29 +52,57 @@ app.get('/admin', routes.adminCollections);
 app.get('/admin/series/:id', routes.adminSeries);
 app.get('/admin/items/:id', routes.adminItems);
 
-app.get('/all', routes.all);
+//Top routes
+//Loads all page with all media objects
+app.get('/all', routes.all); 
+//loads the create page
 app.get('/create', routes.create);
+//loads the edit page
 app.get('/edit', routes.edit);
+//loads the home page
 app.get('/home', routes.home);
-
-app.get('/image/:name/:id', routes.image);
-app.get('/images/:id/list', routes.getItemImages);
-app.get('/item/:id', routes.getItem);
-app.get('/item/:id/remove', routes.removeItem);
-app.get('/media/:id/remove', routes.removeMedia);
-app.get('/items/:id', routes.getItems);
-app.get('/series', routes.getAllSeries);
-//Returns all the series
-app.get('/collections', routes.getAllCollections);
-app.get('/items', routes.getAllItems);
+//----------------------------------------------------------------------------
+//removes a media object with certain id
+//refactor /:type/:id/:command
+app.get('/media/:id/remove', routes.removeMedia); //refactor /media/:id/remove
+//loads the image with certain id
+app.get('/image/:name/:id', routes.image); //refactor /media/:id/get
+//loads all images
+app.get('/images/:id/list', routes.getItemImages);//refactor /media/:id/list
+//----------------------------------------------------------------------------
+//creates a new collection
+//refactor /:type/:id/:command
+app.post('/collection/post', routes.createCollection)//refactor /collection/c/post
+//loads all collections
+app.get('/collections', routes.getAllCollections);//refactor /collection/all/get
+//----------------------------------------------------------------------------
+//creates a new serie
+//refactor /:type/:id/:command
+app.post('/series/create', routes.createSeries)//refactor /series/c/post
+//gets all the series
+app.get('/series', routes.getAllSeries);//refactor /series/all/get
+//----------------------------------------------------------------------------
+//gets all items
+//refactor /:type/:id/:command
+app.get('/items', routes.getAllItems); //refactor /items/:all/:get
+//gets all items from a parentId
+app.get('/items/:id', routes.getItems);//refactor /items/:id/:get
+//----------------------------------------------------------------------------
+//refactor /:type/:id/:command
+//gets an item with a certain id
+app.get('/item/:id', routes.getItem);//refactor /item/:id/:get
+//removes an item with a certain id
+app.get('/item/:id/remove', routes.removeItem);//refactor /item/:id/:get
+//creates an item
+app.post('/item/create', routes.createItem);//refactor /item/c/post
+//----------------------------------------------------------------------------
+//refactor /:type/:id/update
+//updates an object(series/collection or item)
+app.post('/post', routes.data); //refactor /update
+//----------------------------------------------------------------------------
 app.get('/fedora', routes.fedora);
 app.get('/fedora/list', routes.fedoraList);
 app.get('/fedora/:id/approve', routes.fedoraCreateObject);
-
-app.post('/item/create', routes.createItem);
-app.post('/collection/post', routes.createCollection)
-app.post('/series/create', routes.createSeries)
-app.post('/post', routes.data);
 
 // Redirects
 app.get('/', function(req, res) {
