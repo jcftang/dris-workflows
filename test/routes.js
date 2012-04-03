e9c6abb203b48dd4c42b99dc0f43166e5bf17fbc
Quirijn Groot Bluemink <Quirijn.Groot.Bluemink@howest.be>
2012-04-03 15:58:19 +0100

    Updated makefile, removed old tests

--- test/routes.js
+++ test/routes.js
@@ -1,85 +1,64 @@
 var assert = require('chai').assert;
+var request = require('request');
 var app = require('../app');
-process.env.NODE_ENV = 'test';
 
-module.exports = {
-	'GET /home' : function() {
-		assert.ok(app, {
-			url : '/home'
-		}, {
-			status : 200,
-			headers : {
-				'Content-Type' : 'text/html; charset=utf-8'
-			}
-		}, function(res) {
-			assert.includes(res.body, '<title>DRIS Workflows</title>');
-			assert.ok(res);
+describe('My Server', function() {
+	describe('GET /home', function() {
+		it("should respond with the home site", function(done) {
+			request('http://localhost:3000/home', function(err, resp, body) {
+				assert.isNull(err);
+				assert.include(body, '<title>DRIS Workflows</title>');
+				done();
+			});
 		});
-	},
-	'GET /all' : function() {
-		assert.ok(app, {
-			url : '/all'
-		}, {
-			status : 200,
-			headers : {
-				'Content-Type' : 'text/html; charset=utf-8'
-			}
-		}, function(res) {
-			assert.includes(res.body, '<title>All</title>');
-			assert.ok(res);
+	});
+	describe('GET /all', function() {
+		it("should respond with the all site", function(done) {
+			request('http://localhost:3000/all', function(err, resp, body) {
+				assert.isNull(err);
+				assert.include(body, '<title>All</title>');
+				done();
+			});
 		});
-	},
-	'GET /create' : function() {
-		assert.ok(app, {
-			url : '/create'
-		}, {
-			status : 200,
-			headers : {
-				'Content-Type' : 'text/html; charset=utf-8'
-			}
-		}, function(res) {
-			assert.includes(res.body, '<title>Create</title>');
-			assert.ok(res);
+	});
+	describe('GET /create', function() {
+		it("should respond with the create site", function(done) {
+			request('http://localhost:3000/create', function(err, resp, body) {
+				assert.isNull(err);
+				assert.include(body, '<title>Create</title>');
+				done();
+			});
 		});
-	},
-	'GET /edit' : function() {
-		assert.ok(app, {
-			url : '/edit'
-		}, {
-			status : 200,
-			headers : {
-				'Content-Type' : 'text/html; charset=utf-8'
-			}
-		}, function(res) {
-			assert.includes(res.body, '<title>Edit</title>');
-			assert.ok(res);
+	});
+	describe('GET /edit', function() {
+		it("should respond with the edit site", function(done) {
+			request('http://localhost:3000/edit', function(err, resp, body) {
+				assert.isNull(err);
+				assert.include(body, '<title>Edit</title>');
+				done();
+			});
 		});
-	},
-	'GET /admin' : function() {
-		assert.ok(app, {
-			url : '/admin'
-		}, {
-			status : 200,
-			headers : {
-				'Content-Type' : 'text/html; charset=utf-8'
-			}
-		}, function(res) {
-			assert.includes(res.body, '<title>Collections - Admin - DRIS Workflows</title>');
-			assert.ok(res);
+	});
+	describe('GET /admin', function() {
+		it("should respond with the admin site", function(done) {
+			request('http://localhost:3000/admin', function(err, resp, body) {
+				assert.isNull(err);
+				assert.include(body, 'Admin - DRIS Workflows</title>');
+				done();
+			});
 		});
-	},
-	'POST /object/:type/:id/:command' : function() {
-		assert.response(app, {
-			url : '/object/collection/c/post',
-			method : 'POST'
-		}, {
-			body : '{"Title":"RoutesAutobot"}',
-			status : 200,
-			headers : {
-				'Content-Type' : 'text/html; charset=utf-8'
-			}
-		}, function(res) {
-			assert.includes(res.body, 'ysrtu4');
+	});
+	describe('POST /object/:type/:id/:command', function() {
+		it("should respond with the create site", function(done) {
+			request({
+				method:'POST',
+				uri: 'http://localhost:3000/object/collection/c/post',
+				body: '{"Title":"RoutesAutobot"}'
+			}, function(err, resp, body) {
+				assert.isNull(err);
+				assert.include(body, 'create');
+				done();
+			});
 		});
-	}
-}
+	});
+});
