4d3730576f073812859ce56d7bb9186da9671ca0
Quirijn Groot Bluemink <Quirijn.Groot.Bluemink@howest.be>
2012-04-03 16:24:56 +0100

    Refactored app.js.

--- test/routes.js
+++ test/routes.js
@@ -1,11 +1,24 @@
 var assert = require('chai').assert;
 var request = require('request');
-var app = require('../app');
+
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var appRoutes = require('../app-routes');
+var appConfig = require('../app-config');
+var app = module.exports = express.createServer();
+
+appConfig.configure(app);
+appRoutes.createRoutes(app);
+app.listen(7000);
+
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 describe('My Server', function() {
 	describe('GET /home', function() {
 		it("should respond with the home site", function(done) {
-			request('http://localhost:3000/home', function(err, resp, body) {
+			request('http://localhost:7000/home', function(err, resp, body) {
 				assert.isNull(err);
 				assert.include(body, '<title>DRIS Workflows</title>');
 				done();
@@ -14,7 +27,7 @@ describe('My Server', function() {
 	});
 	describe('GET /all', function() {
 		it("should respond with the all site", function(done) {
-			request('http://localhost:3000/all', function(err, resp, body) {
+			request('http://localhost:7000/all', function(err, resp, body) {
 				assert.isNull(err);
 				assert.include(body, '<title>All</title>');
 				done();
@@ -23,7 +36,7 @@ describe('My Server', function() {
 	});
 	describe('GET /create', function() {
 		it("should respond with the create site", function(done) {
-			request('http://localhost:3000/create', function(err, resp, body) {
+			request('http://localhost:7000/create', function(err, resp, body) {
 				assert.isNull(err);
 				assert.include(body, '<title>Create</title>');
 				done();
@@ -32,7 +45,7 @@ describe('My Server', function() {
 	});
 	describe('GET /edit', function() {
 		it("should respond with the edit site", function(done) {
-			request('http://localhost:3000/edit', function(err, resp, body) {
+			request('http://localhost:7000/edit', function(err, resp, body) {
 				assert.isNull(err);
 				assert.include(body, '<title>Edit</title>');
 				done();
@@ -41,7 +54,7 @@ describe('My Server', function() {
 	});
 	describe('GET /admin', function() {
 		it("should respond with the admin site", function(done) {
-			request('http://localhost:3000/admin', function(err, resp, body) {
+			request('http://localhost:7000/admin', function(err, resp, body) {
 				assert.isNull(err);
 				assert.include(body, 'Admin - DRIS Workflows</title>');
 				done();
@@ -52,7 +65,7 @@ describe('My Server', function() {
 		it("should respond with the create site", function(done) {
 			request({
 				method:'POST',
-				uri: 'http://localhost:3000/object/collection/c/post',
+				uri: 'http://localhost:7000/object/collection/c/post',
 				body: '{"Title":"RoutesAutobot"}'
 			}, function(err, resp, body) {
 				assert.isNull(err);
