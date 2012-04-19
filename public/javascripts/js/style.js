e6990c609250a6746ea848235c5ebd5bc8170933
matthias van wambeke <matthias.van.wambeke@howest.be>
2012-04-19 13:02:34 +0100

    sortable fix

--- public/javascripts/js/style.js
+++ public/javascripts/js/style.js
@@ -43,7 +43,7 @@ function loadMediaData(){
 */
 function editAction(){
 
-	$( "form" ).sortable({items:'div.control-group'});
+	$( "form" ).not("#step1 form").sortable({items:'div.control-group'});
 	//loadAllItems();
 	//hides the box which allows users to upload files
 	$("#fileBox").hide();
