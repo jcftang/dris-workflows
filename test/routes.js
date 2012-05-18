1b552079bb1cfbd251ce69d94acd8af75daafae4
Quirijn Groot Bluemink <Quirijn.Groot.Bluemink@howest.be>
2012-05-18 11:00:24 +0100

    Added test for 404 page

--- test/routes.js
+++ test/routes.js
@@ -9,16 +9,17 @@ var appRoutes = require('../app-routes');
 var appConfig = require('../app-config');
 var app = module.exports = express.createServer();
 
+var portNumber = 7000
 appConfig.configure(app);
 appRoutes.createRoutes(app);
-app.listen(7000);
+app.listen(portNumber);
 
 //console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 describe('Try accessing top level Routes', function() {
 	describe('GET /home', function() {
 		it("should respond with the home page", function(done) {
-			request('http://localhost:7000/home', function(err, resp, body) {
+			request('http://localhost:' + portNumber + '/home', function(err, resp, body) {
 				assert.isNull(err);
 				assert.include(body, '<title>DRIS Workflows</title>');
 				done();
@@ -27,7 +28,7 @@ describe('Try accessing top level Routes', function() {
 	});
 	describe('GET /all', function() {
 		it("should respond with the all page", function(done) {
-			request('http://localhost:7000/all', function(err, resp, body) {
+			request('http://localhost:' + portNumber + '/all', function(err, resp, body) {
 				assert.isNull(err);
 				assert.include(body, '<title>All</title>');
 				done();
@@ -36,7 +37,7 @@ describe('Try accessing top level Routes', function() {
 	});
 	describe('GET /create', function() {
 		it("should respond with the create page", function(done) {
-			request('http://localhost:7000/create', function(err, resp, body) {
+			request('http://localhost:' + portNumber + '/create', function(err, resp, body) {
 				assert.isNull(err);
 				assert.include(body, '<title>Create</title>');
 				done();
@@ -45,7 +46,7 @@ describe('Try accessing top level Routes', function() {
 	});
 	describe('GET /edit', function() {
 		it("should respond with the edit page", function(done) {
-			request('http://localhost:7000/edit', function(err, resp, body) {
+			request('http://localhost:' + portNumber + '/edit', function(err, resp, body) {
 				assert.isNull(err);
 				assert.include(body, '<title>Edit</title>');
 				done();
@@ -54,9 +55,18 @@ describe('Try accessing top level Routes', function() {
 	});
 	describe('GET /admin', function() {
 		it("should respond with the admin page", function(done) {
-			request('http://localhost:7000/admin', function(err, resp, body) {
+			request('http://localhost:' + portNumber + '/admin', function(err, resp, body) {
 				assert.isNull(err);
 				assert.include(body, '<title>Admin</title>');
+				done();
+			});
+		});
+	});
+	describe('GET /404', function() {
+		it("should respond with the admin page", function(done) {
+			request('http://localhost:' + portNumber + '/sdfagfgarg', function(err, resp, body) {
+				assert.isNull(err);
+				assert.include(body, '<title>404 - Error</title>');
 				done();
 			});
 		});
